bbd04640eb6310b9fd08477cf33f7009e021e846
James McDonald <jamesmcdonald112@yahoo.ie>
2025-04-13 12:54:41 +0100

    Set up Vite + React + Tailwind v4.1 plugin

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl text-indigo-600 font-bold underline">
+      ✅ Tailwind v4.1 plugin setup works!
+    </h1>
+    </>
+  )
+}
+
+export default App
